4e1705dcc13ef454d72ff1c734ab39b6d20df65f
altheaiyah <altheamishkaalvarez@gmail.com>
2025-11-26 12:21:08 +0800

    Add profiles.js and albums.js

---
+++ albums.js
@@ -0,0 +1,128 @@
+var albums = [
+  {
+    "picture": "img/bts album/a21.jpg", 
+    "type": "1st Live Album", "title": "PERMISSION TO DANCE ON STAGE-LIVE",
+    "tracklist": "<li>ON-Live</li> <li>Burning Up (FIRE)-Live</li> <li>Dope-Live</li> <li>DNA-Live</li> <li>Blue & Grey-Live</li> <li>Black Swan-Live</li> <li>Blood Sweat & Tears-Live</li> <li>Fake Love-Live</li> <li>Life Goes On-Live</li> <li>Boy With Luv (feat. Halsey)-Live</li> <li>Dynamite-Live</li> <li>Butter-Live</li> <li>Telepathy-Live</li> <li>Outro:Wings-Live</li> <li>Stay-Live</li> <li>So What-Live</li> <li>Idol-Live</li> <li>Airplane pt.2-Live</li> <li>Silver Spoon-Live</li> <li>Dis-ease-Live</li> <li>Spring Day-Live</li> <li>Permission to Dance-Live</li>", 
+    "description":  "The notable release captures the dynamic energy of their 'BTS PERMISSION TO DANCE ON STAGE' tour, held across Seoul, Los Angeles, and Las Vegas in 2021. Spanning 12 unforgettable shows that offers an immersive glimpse into BTS' electrifying global stage presence." 
+  },
+  {
+    "picture": "img/bts album/a20.png", 
+    "type": " ", "title": "TAKE TWO",
+    "tracklist": "<li>Take Two</li>",
+    "description":  "Take Two is a digital single celebrating the band's 10th anniversary, on June 9. “Take Two” is an acoustic pop genre song. BTS look back on their decade-long journey since their debut, and picture the forthcoming future together with their fans."
+  },
+  {
+    "picture": "img/bts album/a19.png", 
+    "type": "1st Anthology Album", "title": "PROOF",
+    "tracklist": "<h4 class=fw-bold>CD1</h4> <li>Born Singer</li> <li>No More Dream</li> <li>N.O</li> <li>Boy In Luv</li> <li>Danger</li> <li>I NEED U</li> <li>RUN</li> <li>Burning Up (FIRE)</li> <li>Blood Sweat & Tears</li> <li>Spring Day</li> <li>DNA</li> <h4 class=fw-bold><br>CD2</h4> <li>Run BTS</li> <li>Intro: Persona</li> <li>Stay</li> <li>Moon</li> <li>Jamais Vu</li> <li>Trivia 承: Seesaw</li> <li>BTS Cypher PT.3: KILLER (Feat. Supreme Boi)</li> <li>Outro: Ego</li> <li>Her</li> <li>Filter</li> <h4 class=fw-bold><br>CD3</h4> <li>Jump (Demo Ver.)</li> <li>Young Love</li> <li>Boy In Luv (Demo Ver.)</li> <li>Quotation Mark</li> <li>I NEED U (Demo Ver.)</li> <li>Boyz with Fun (Demo Ver.)</li> <li>Tony Montana (with Jimin)</li> <li>Young Forever (RM Demo Ver.)</li> <li>Spring Day (V Demo Ver.)</li> <li>DNA (j-hope Demo Ver.)</li> <li>Epiphany (Jin Demo Ver.)</li>",
+    "description":  "The new anthology album Proof is the core of BTS' history. The album comprises 48 tracks total, from the greatest hit songs, and solo/sub-unit tracks selected by each member, unreleased tracks to special tracks dedicated to their fans ARMY."
+  },
+  {
+    "picture": "img/bts album/a18.jpg", 
+    "type": "CD Single", "title": "BUTTER",
+    "tracklist": "<li>Butter</li> <li>Permission to Dance</li> <li>Butter (Instrumental)</li> <li>Permission to Dance (Instrumental)</li>",
+    "description":  "The CD single includes the shimmering summer song “Butter” as well as “Permission to Dance” that will get your heart pumping and dance again. World-renowned musician Ed Sheeran and British producer Steve Mac took part in making the song."
+  },
+  {
+    "picture": "img/bts album/a17.jpg", 
+    "type": "Digital Single", "title": "BUTTER",
+    "tracklist": "<li>Butter</li> <li>Butter (Instrumental)</li>",
+    "description":  "BTS' latest summer anthem “Butter” released on May 21 is a dance pop track and their second English song following “Dynamite”. The fun and witty lyrics brimming with positive and bright energy will instantly uplift moods."
+  },
+  {
+    "picture": "img/bts album/a16.jpg", 
+    "type": "", "title": "BE",
+    "tracklist": "<li>Life Goes On</li> <li>Fly to My Room</li> <li>Blue & Grey</li> <li>Skit</li> <li>Telepathy</li> <li>Dis-ease</li> <li>Stay</li> <li>Dynamite</li>",
+    "description":  "BTS unfold another chapter of 2020 with their new album BE. The album consists of a total of eight tracks with a lead single “Life Goes On”. The lead singe speaks on behalf of everyone stuck in the new normality brought on by COVID-19. BTS echoes in a low voice the comforting message that “Life Goes On” and extends their hands towards the future."
+  },
+  {
+    "picture": "img/bts album/a15.jpg", 
+    "type": "Digital Single", "title": "DYNAMITE",
+    "tracklist": "<li>Dynamite</li> <li>Dynamite (Instrumental)</li> <li>Dynamite (Acoustic Remix)</li> <li>Dynamite (EDM Remix)</li>",
+    "description":  "A Disco Pop single and marks as BTS' first song to be released completely in English. The choreography is edgy and upbeat, with deeply addicting movements that highlight the distinct charms of each BTS member."
+  },
+  {
+    "picture": "img/bts album/a14.jpg", 
+    "type": "4th Full Length Album", "title": "MAP OF THE SOUL: 7",
+    "tracklist": "<li>Intro: Persona</li> <li>Boy With Luv (Feat. Halsey)</li> <li>Make It Right</li> <li>Jamais Vu</li> <li>Dionysus</li> <li>Interlude: Shadow</li> <li>Black Swan</li> <li>Filter</li> <li>My Time</li> <li>Louder than bombs</li> <li>ON</li> <li>UGH!</li> <li>00:00 (Zero O'Clock)</li> <li>Inner Child</li> <li>Friends</li> <li>Moon</li> <li>Respect</li> <li>We are Bulletproof: the Eternal</li> <li>Outro: Ego</li>",
+    "description":  "A heartfelt story of BTS and their search for their “real selves”. “MAP OF THE SOUL: 7” looks back on the past seven years of BTS since their debut as seven members of one team. The first chapter of this story, “MAP OF THE SOUL: PERSONA”, sang about the world and the joy of love, and the discovery of the self as it is displayed to the world; “MAP OF THE SOUL: 7” now turns inward to the shadow within in a candid acknowledgement that it, too, is part of the self."
+  },
+  {
+    "picture": "img/bts album/a13.jpg", 
+    "type": "6th Mini Album", "title": "MAP OF THE SOUL: PERSONA",
+    "tracklist": "<li>Intro: Persona</li> <li>Boy With Luv (Feat. Halsey)</li> <li>Mikrokosmos</li> <li>Make It Right</li> <li>HOME</li> <li>Jamais Vu</li> <li>Dionysus</li>",
+    "description":  "BTS talks about the joy in finding love, and reaches out to the world with the messages they want to share with their fans. A total of 7 tracks in the album, including RM's  'Intro: Persona', lead single 'Boy With Luv (Feat. Halsey)', and the collaboration with world-famous singer-songwriter Ed Sheeran, 'Make It Right'."
+  },
+  {
+    "picture": "img/bts album/a12.jpg", 
+    "type": "Repackaged Album", "title": "LOVE YOURSELF 結 'ANSWER'",
+    "tracklist":"<h4 class=fw-bold><br>A</h4> <li>Euphoria</li> <li>Trivia 起: Just Dance</li> <li>Serendipity (Full Length Edition)</li> <li>DNA</li> <li>Dimple</li> <li>Trivia 承: Love</li> <li>Her</li> <li>Singularity</li> <li>FAKE LOVE</li> <li>The Truth Untold (Feat. Steve Aoki)</li> <li>Trivia 承: Seesaw</li> <li>Tear</li> <li>Epiphany</li> <li>I'm Fine</li> <li>IDOL</li> <li>Anser: Love Myself</li> <h4 class=fw-bold><br>B</h4> <li>Magic Shop</li> <li>Best Of Me</li> <li>Airplane pt.2</li> <li>Go Go</li> <li>Anpanman</li> <li>MIC Drop</li> <li>DNA (Pedal 2 LA Mix)</li> <li>FAKE LOVE (Rocking Vibe Remix)</li> <li>MIC Drop (Steve Aoki Remix)(Full Length Edition)</li>",
+    "description":  "BTS, LOVE YOURSELF 結 'ANSWER' “Loving oneself is where true love begins” A celebration with the ARMY. 'ANSWER' is the final piece of the puzzle. Though it’s a repackaged album, LOVE YOURSELF 結 'ANSWER' still includes seven brand-new tracks. CD A is a concept album, its sixteen tracks all connected lyrically to highlight BTS' prowess as both artists and storytellers."
+  },
+  {
+    "picture": "img/bts album/a11.jpg", 
+    "type": "3rd Full-Length Album", "title": "LOVE YOURSELF 轉 'TEAR'",
+    "tracklist": "<li>Intro: Singularity</li> <li>FAKE LOVE</li> <li>The Truth Untold (Feat. Steve Aoki)</li> <li>134340</li> <li>Paradise</li> <li>Love Maze</li> <li>Magic Shop</li> <li>Airplane pt.2</li> <li>Anpanman</li> <li>So What</li> <li>Outro: Tear</li>",
+    "description":  "BTS releases third studio album LOVE YOURSELF 轉 'TEAR' Fake love inevitably leads to loss and separation, For true love can only begin when you learn to love yourself. LOVE YOURSELF 起 'ANSWER' and LOVE YOURSELF 承 'HER' albums expressed the heart-fluttering and tingling moments of love, LOVE YOURSELF 轉 'TEAR' album embodies the pain of boys faced with parting."
+  },
+  {
+    "picture": "img/bts album/a10.jpg", 
+    "type": "5th Mini Album", "title": "LOVE YOURSELF 承 'HER'",
+    "tracklist": "<li>Intro: Serendipity</li> <li>DNA</li> <li>Best Of Me</li> <li>Dimple</li> <li>Pied Piper</li> <li>Skit: Billboard Music Awards Speech</li> <li>MIC Drop</li> <li>Go Go </li> <li>Outro: Her</li>",
+    "description":  "BTS releases LOVE YOURSELF 承 'HER'! “Loving yourself is the beginning of true love”, the 5th mini album LOVE YOURSELF 承 'HER' is an expression of the anxiety and elation of love, told in the unique style of BTS. The album begins this new narrative, the image of boys in love for the first time are expressed in a refreshing and cheerful take that aims to talk about the topic of “love” while immersed in a coming-of-age tale."
+  },
+  {
+    "picture": "img/bts album/a9.jpg", 
+    "type": "2nd Special Album", "title": "YOU NEVER WALK ALONE",
+    "tracklist": "<li>Intro: Boy Meets Evil</li> <li>Blood Sweat & Tears </li> <li>Begin</li> <li>Lie</li> <li>Stigma</li> <li>First Love</li> <li>Reflection</li> <li>MAMA</li> <li>Awake</li> <li>Lost</li> <li>BTS Cypher 4</li> <li>Am I Wrong</li> <li>21st Century Girl</li> <li>2! 3!</li> <li>Spring Day</li> <li>Not Today</li> <li>Outro: Wings</li> <li>A Supplementary Story: You Never Walk Alone</li>",
+    "description":  "YOU NEVER WALK ALONE completes the stories of youth and growth that could not all be contained in the WINGS album. Whereas The most beautiful moment in life series and WINGS were narratives of youth and growth, this additional chapter to WINGS is a message of warm consolation and hope for the suffering youth of this generation."
+  },
+  {
+    "picture": "img/bts album/a8.jpg", 
+    "type": "2nd Full-Length Album", "title": "WINGS",
+    "tracklist": "<li>Intro: Boy Meets Evil</li> <li>Blood Sweat & Tears </li> <li>Begin</li> <li>Lie</li> <li>Stigma</li> <li>First Love</li> <li>Reflection</li> <li>MAMA</li> <li>Awake</li> <li>Lost</li> <li>BTS Cypher 4</li> <li>Am I Wrong</li> <li>21st Century</li> <li>2! 3!</li> <li>Interlude: Wings</li>",
+    "description":  "WINGS contains songs about boys who encounter temptation for the first time and must ponder and agonize in the face of it. As the seven boys experience pain and bliss, they bring forth the image of birds that break out of their shells and try to take flight for the first time."
+  },
+  {
+    "picture": "img/bts album/a7.jpg", 
+    "type": "1st Special Album", "title": "THE MOST BEAUTIFUL MOMENT IN LIFE: YOUNG FOREVER",
+    "tracklist":"<b>A</b> <li>INTRO: The most beautiful moment in life</li> <li>I NEED U</li> <li>Hold Me Tight</li> <li>DNA</li> <li>Autumn Leaves</li> <li>Butterfly prologue mix</li> <li>RUN</li> <li>Ma City</li> <li>Silver Spoon</li> <li>DOPE</li> <li>Burning Up (FIRE)</li> <li>Save ME</li> <li>Epilogue: Young Forever</li> <b>B</b> <li>Converse High</li> <li>Moving On</li> <li>Whalien 52</li> <li>Butterfly</li> <li>House OF Cards (Full Length Edition)</li> <li>Love is Not Over (Full Length Edition)</li> <li>I NEED U (Urban Mix)</li> <li>I NEED U (Remix)</li> <li>RUN (Ballad Mix)</li> <li>RUN (Alternative Mix)</li> <li>Butterfly (Alternative Mix)</li>",
+    "description":  "The final chapter of the story of The most beautiful moment in life. True to the title of the albums, the series has enabled BTS to now be in the most beautiful moment of their lives. The most beautiful moment in life: YOUNG FOREVER is a special album that marks the conclusion of the epic journey of the series, containing the last stories told by young people who, despite an uncertain and insecure reality (The most beautiful moment in life pt.1) continue to surge forward (The most beautiful moment in life pt.2)."
+  },
+  {
+    "picture": "img/bts album/a6.jpg", 
+    "type": "4th Mini Album", "title": "THE MOST BEAUTIFUL MOMENT IN LIFE, PT. 2",
+    "tracklist": "<li>INTRO: Never Mind</li> <li>RUN</li> <li>Butterfly</li> <li>Whalien 52</li> <li>Ma City</li> <li>Silver Spoon</li> <li>SKIT: One night in a strange city</li> <li>Autmun Leaves</li> <li>OUTRO: House of Cards</li>",
+    "description":  " The most beautiful moment in life pt.2 sings about the energy of youth barreling forward, embracing even the uncertainty and insecurity. Now beginning to taste the bitterness of grown-up life, they can easily sense that life isn't always beautiful. The “Youth Series” concludes with The most beautiful moment in life pt.2, but the boys are only now on the starting line. “Never Mind”, BTS is just getting started."
+  },
+  {
+    "picture": "img/bts album/a5.jpg", 
+    "type": "3rd Mini Album", "title": "THE MOST BEAUTIFUL MOMENT IN LIFE, PT. 1",
+    "tracklist": "<li>INTRO: The Most Beautiful Moment In Life</li> <li>I NEED U</li> <li>Hold Me Tight</li> <li>SKIT: Expectation!</li> <li>DOPE</li> <li>Boyz with Fun</li> <li>Converse High</li> <li>Moving On</li> <li>OUTRO: Love Is Not Over</li>",
+    "description":  "No longer just young boys, the album is an introduction to BTS as a group that continues to grow and develop. After successfully concluding the “School Trilogy” that discusses the main topics of interest for teens—dreams, happiness and love—the new album begins to discuss “youth”. BTS has chosen the theme of “the most beautiful moment in life”, the start of one’s young adulthood in which beauty coexists with uncertainty. The most beautiful moment in life pt.1 focuses on the uncertain future more than the glamour of youth."
+  },
+  {
+    "picture": "img/bts album/a4.jpg", 
+    "type": "1st Full-Length Album", "title": "DARK & WILD",
+    "tracklist": "<li>Intro: What am I to you</li> <li>Danger</li> <li>War of Hormone</li> <li>Hip Hop Phile</li> <li>Let Me Know</li> <li>Rain</li> <li>BTS Cypher PT.3: KILLER (Feat. Supreme Boi)</li> <li>Interlude: What are you doing now</li> <li>Could you turn off your cell phone</li> <li>Embarrassed</li> <li>24/7=heaven</li> <li>Look here</li> <li>So 4 more</li> <li>Outro: Do you think it makes sense?</li>",
+    "description":  "BTS sings about love gone awry in DARK & WILD. The boys are no longer in their school uniforms. They have become tougher, singing that their love is not to be treated lightly. The title track “Danger” is a tightly-composed hybrid hip hop song with a piercing punk-rock guitar sound that features stark rap, a powerful hook and exhilarating shouting."
+  },
+  {
+    "picture": "img/bts album/a3.jpg", 
+    "type": "2nd Mini Album", "title": "SKOOL LUV AFFAIR",
+    "tracklist": "<li>Intro: Skool Luv Affair</li> <li>Boy In Luv</li> <li>Skit: Soulmate</li> <li>Where You From</li> <li>Just One Day</li> <li>Tomorrow</li> <li>BTS Cypher Pt. 2: Triptych</li> <li>Spine Breaker</li> <li>Jump</li> <li>Outro: Propose</li>",
+    "description":  "SKOOL LUV AFFAIR, the album’s main theme is the love that blossoms in the school. This album is a culmination of the “School Trilogy” project that discusses the topics of most interest to teens their own age—dreams, happiness and love. Such penetrating insight into the hearts and minds of young people led to the absolute support that BTS has received from fans their age."
+  },
+  {
+    "picture": "img/bts album/a2.jpg", 
+    "type": "1st Mini Album", "title": "O!RUL8,2?",
+    "tracklist": "<li>INTRO: O!RUL8,2?</li> <li>N.O</li> <li>We On</li> <li>Skit: R U Happy Now?</li> <li>If I Ruled the World</li> <li>Coffee</li> <li>BTS Cypher PT.1</li> <li>Attack on Bangtan</li> <li>Paldogangsan</li> <li>OUTRO: LUV IN SKOOL</li>",
+    "description":  "BTS offers the message that “you must find your happiness and your own life before it's too late.” Lyrics that brutally lay open reality as it is deliver a bold admonition to a world that inflicts despair. Children are raised as studying machines without a chance to think about the future. A world where classmates are not friends but competitors trying to trample each other. BTS sings about the wounds and anguish that such hopeless life has left on today’s teens."
+  },
+  {
+    "picture": "img/bts album/a1.jpg", 
+    "type": "1st Single Album", "title": "2 COOL 4 SKOOL",
+    "tracklist": "<li>Intro: 2 COOL 4 SKOOL (Feat. DJ Friz)</li> <li>We Are Bulletproof Pt.2</li> <li>Skit: Circle Room Talk</li> <li>No More Dream</li> <li>Interlude</li> <li>Like</li> <li>Outro: Circle Room Cypher</li>",
+    "description": "Hip hop group BTS has released its debut single 2 COOL 4 SKOOL. This is the product of the firm conviction and passion of BTS that hip hop musicians must make their voices heard through their albums. There is no discontinuity between the emotions and lyrics of BTS. The album is a satisfaction in both quality and quantity."
+  }
+]
---
+++ profiles.js
@@ -0,0 +1,58 @@
+var profiles = [
+  {
+    "picture": "img/members/member-rm.jpg", 
+    "stageName": "RM",
+    "positions": "Leader, Main Rapper", 
+    "birthName": "Kim Nam-joon (김남준)",
+    "birthday": "September 12, 1994",
+    "representativeEmoji": "🐨",
+  },
+  {
+    "picture": "img/members/member-jin.jpg", 
+    "stageName": "JIN",
+    "positions": "Sub-Vocalist, Visual", 
+    "birthName": "Kim Seok-jin (김석진)",
+    "birthday": "December 4, 1992",
+    "representativeEmoji": "🐹/🦙"
+  },
+  {
+    "picture": "img/members/member-suga.jpg", 
+    "stageName": "SUGA",
+    "positions": "Lead Rapper", 
+    "birthName": "Min Yoon-gi (민윤기)",
+    "birthday": "March 9, 1993",
+    "representativeEmoji": "🐱"
+  },
+  {
+    "picture": "img/members/member-jhope.jpg", 
+    "stageName": "J-HOPE",
+    "positions": "Main Dancer, Sub-Rapper/Vocalist", 
+    "birthName": "Jung Ho-seok (정호석)",
+    "birthday": "February 18, 1994",
+    "representativeEmoji": "🐿️"
+  },
+  {
+    "picture": "img/members/member-jimin.jpg", 
+    "stageName": "JIMIN",
+    "positions": "Main Dancer, Lead Vocalist", 
+    "birthName": "Park Ji-min (박지민)",
+    "birthday": "October 13, 1995",
+    "representativeEmoji": "🐣/🐥"
+  },
+  {
+    "picture": "img/members/member-v.jpg", 
+    "stageName": "V",
+    "positions": "Sub-Vocalist, Visual", 
+    "birthName": "Kim Tae-hyung (김태형)",
+    "birthday": "December 30, 1995",
+    "representativeEmoji": "🐻/🐯"
+  },
+  {
+    "picture": "img/members/member-jk.jpg", 
+    "stageName": "JUNG KOOK",
+    "positions": "Main Vocalist, Lead Dancer, Sub-Rapper, Center", 
+    "birthName": "Jeon Jung-kook (전정국)",
+    "birthday": "September 1, 1997",
+    "representativeEmoji": "🐰"
+  },
+]
