e53b9fe5114af1cc0ba0ca1460417cf98d537ed7
altheaiyah <altheamishkaalvarez@gmail.com>
2025-11-26 18:47:36 +0800

    Add information and images; update about and index pages

---
+++ content.js
@@ -0,0 +1,22 @@
+var lightstick = [
+  {
+    "picture": "img/lightstick/v1.jpg", 
+    "title": "ARMY BOMB VER.1",
+    "description": "This first version was released in 2015. It features a sleek design with a white globe, symbolizing the unity between BTS and their fans, known as ARMY."
+  },
+  {
+    "picture": "img/lightstick/v2.webp", 
+    "title": "ARMY BOMB VER.2",
+    "description": "This second version was released on February 10, 2017 and came with Bluetooth to enable color changes during concerts."
+  },
+  {
+    "picture": "img/lightstick/v3.jpg", 
+    "title": "ARMY BOMB VER.3",
+    "description": "This version was released on July 27, 2018 and it has an official app to switch the light stick to concert and manual mode using Bluetooth."
+  },
+  {
+    "picture": "img/lightstick/v4.webp", 
+    "title": "ARMY BOMB MAP OF THE SOUL SPECIAL EDITION",
+    "description": "This version was released on April 14, 2020 and it has an official app to switch the light stick to concert and manual mode using Bluetooth."
+  }
+]
